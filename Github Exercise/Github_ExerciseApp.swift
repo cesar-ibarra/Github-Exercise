5443d48aece1535a0ef83dcb3082fc3d4dadc1e4
Cesar Ibarra <ibarratcesar@icloud.com>
2024-02-27 06:14:09 -0800

    Initial Commit

---
+++ Github Exercise/Github_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseApp.swift
+//  Github Exercise
+//
+//  Created by Cesar Ibarra on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
